60cbc6ac9e008ffb53584c322ebd75899fa38382
sirak2010@gmail.com <sirak2010@gmail.com@9f98ea4f-23e5-6f14-547c-fb818911484e>
2013-01-25 15:50:56 +0000

    Box Creator Icon changed

--- trunk/SerakTesseractTrainer/SerakTesseractTrainer/Box Creator.Designer.cs
+++ trunk/SerakTesseractTrainer/SerakTesseractTrainer/Box Creator.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Box_Creator));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -134,6 +135,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Box_Creator";
